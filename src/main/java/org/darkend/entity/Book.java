856b602fee8b57a51c02d3767810e883520cadd7
MarcusLeeman <marcus.leeman@iths.se>
2022-04-27 14:53:52 +0200

    Remove annotation: Serdeable

--- src/main/java/org/darkend/entity/Book.java
+++ src/main/java/org/darkend/entity/Book.java
@@ -4,12 +4,10 @@ import io.micronaut.core.annotation.Creator;
 import io.micronaut.core.annotation.Introspected;
 import io.micronaut.core.annotation.NonNull;
 import io.micronaut.core.annotation.ReflectiveAccess;
-import io.micronaut.serde.annotation.Serdeable;
 import org.bson.codecs.pojo.annotations.BsonCreator;
 import org.bson.codecs.pojo.annotations.BsonProperty;
 
 @Introspected
-@Serdeable
 @ReflectiveAccess
 public class Book {
 
